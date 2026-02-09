eeb18e7d75bc454af50c379b88042bdbbee69ba5
Shivanshu <shivanshutyagi789@gmail.com>
2026-02-09 10:41:02 +0530

    Update IssueService.java

--- Library Management system.java/src/IssueService.java
+++ Library Management system.java/src/IssueService.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class IssueService {
     public static void issueBook() {
         try (Connection conn = Database.getConnection()) {
-//            System.out.print("Enter Book ID: ");
+
             int bookId = Integer.parseInt(InutHelper.readInt("Enter Book ID: "));
-//            System.out.print("Enter Member ID: ");
+
             int memberId = Integer.parseInt(InutHelper.readInt("Enter Member ID: "));
             String q = "INSERT INTO issue_return (book_id, member_id, issue_date) VALUES (?, ?, CURDATE())";
             PreparedStatement ps = conn.prepareStatement(q);
@@ -20,7 +20,7 @@ public class IssueService {
     public static void returnBook() {
         try (Connection conn = Database.getConnection()) {
 
-//            System.out.print("Enter Issue Transaction ID: ");
+
             int issueId = Integer.parseInt(InutHelper.readInt("Enter Issue Transcation ID: "));
 
             String q = "UPDATE issue_return SET return_date = CURDATE() WHERE id = ? AND return_date IS NULL";
@@ -34,3 +34,4 @@ public class IssueService {
         }
     }
 }
+
